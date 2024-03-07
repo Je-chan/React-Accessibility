c977260ba1d3fc8b957879d696cb95297badfbb0
ye chan <jkyyc3@gmail.com>
2024-03-07 23:06:07 +0900

    feat: active cell

--- 11_simple-project/index.js
+++ 11_simple-project/index.js
@@ -3,20 +3,56 @@ const ROWS = 10;
 const COLS = 10;
 const spreadSheet = [];
 
+const makeCellId = (row, col) => {
+  return "cell_" + row + col;
+};
+
 class Cell {
-  constructor(isHeader, disabled, data, row, column, active = false) {
+  constructor(
+    isHeader,
+    disabled,
+    data,
+    row,
+    column,
+    rowName,
+    columnName,
+    active = false,
+  ) {
     this.isHeader = isHeader;
     this.disabled = disabled;
     this.data = data;
     this.row = row;
     this.column = column;
+    this.rowName = rowName;
+    this.columnName = columnName;
     this.active = active;
   }
 
   getCellId() {
-    return "cell_" + this.row + this.column;
+    return makeCellId(this.row, this.column);
   }
 }
+
+const getElementFromRowCol = (row, col) => {
+  return document.querySelector("#" + makeCellId(row, col));
+};
+
+const handleCellClick = (cell) => {
+  const columnHeader = spreadSheet[0][cell.column];
+  const rowHeader = spreadSheet[cell.row][0];
+
+  const columnHeaderElement = getElementFromRowCol(
+    columnHeader.row,
+    columnHeader.column,
+  );
+  const rowHeaderElement = getElementFromRowCol(
+    rowHeader.row,
+    rowHeader.column,
+  );
+
+  columnHeaderElement.classList.add("active");
+  rowHeaderElement.classList.add("active");
+};
 
 const createCellElement = (cell) => {
   const cellElement = document.createElement("input");
@@ -24,6 +60,12 @@ const createCellElement = (cell) => {
   cellElement.id = cell.getCellId();
   cellElement.value = cell.data;
   cellElement.disabled = cell.disabled;
+
+  if (cell.isHeader) {
+    cellElement.classList.add("header");
+  }
+
+  cellElement.onclick = () => handleCellClick(cell);
 
   return cellElement;
 };
@@ -43,14 +85,44 @@ const drawSheet = () => {
   }
 };
 
+const getCellPropertyValue = (row, col) => {
+  const cellPropertyValue = {
+    rowName: row || "",
+    columnName: col ? String.fromCharCode(64 + col) : "",
+  };
+  if (row === 0) {
+    cellPropertyValue.isHeader = true;
+    cellPropertyValue.disabled = true;
+    cellPropertyValue.data = cellPropertyValue.columnName;
+  } else if (col === 0) {
+    cellPropertyValue.isHeader = true;
+    cellPropertyValue.disabled = true;
+    cellPropertyValue.data = cellPropertyValue.rowName;
+  } else {
+    cellPropertyValue.isHeader = false;
+    cellPropertyValue.disabled = false;
+    cellPropertyValue.data = "";
+  }
+
+  return cellPropertyValue;
+};
+
 const initSpreadSheet = () => {
   for (let row = 0; row < ROWS; row++) {
     const cols = [];
     for (let col = 0; col < COLS; col++) {
-      let cellData = "";
-      if (col === 0) cellData = row || "";
-      if (row === 0) cellData = col ? String.fromCharCode(64 + col) : "";
-      const cell = new Cell(false, false, cellData, row, col, false);
+      const { isHeader, disabled, data, rowName, columnName } =
+        getCellPropertyValue(row, col);
+      const cell = new Cell(
+        isHeader,
+        disabled,
+        data,
+        row,
+        col,
+        rowName,
+        columnName,
+        false,
+      );
       cols.push(cell);
     }
     spreadSheet.push(cols);
