7e37d10cb27b897c170f1361e8a8d22074ba609e
ye chan <jkyyc3@gmail.com>
2024-03-08 23:46:52 +0900

    chore : 주석

--- 11_simple-project/index.js
+++ 11_simple-project/index.js
@@ -3,10 +3,27 @@ const ROWS = 10;
 const COLS = 10;
 const spreadSheet = [];
 
+/**
+ * Cell 을 담당하는 HTML 태그의 #id 값을 생성하는 함수
+ * @param row : cell 의 row 위치
+ * @param col : cell 의 column 위치
+ * @returns {string} : 해당 row-column 에 위치한 CELl 의 ID
+ */
 const makeCellId = (row, col) => {
   return "cell_" + row + col;
 };
 
+/**
+ * Cell 의 클래스
+ * isHeader : 헤더 셀인가?
+ * disabled : 비활성화
+ * data : Cell 의 데이터
+ * row : 몇 번째 row 에 위치하는지
+ * column : 몇 번째 column 에 위치하는지
+ * rowName : row 의 이름(Header 에 적힌 값)
+ * columnName : column 의 이름(Header 에 적힌 값)
+ * active : 활성화 상태인지 (사용자가 선택한 셀인지)
+ */
 class Cell {
   constructor(
     isHeader,
@@ -28,16 +45,40 @@ class Cell {
     this.active = active;
   }
 
+  // 이 셀의 cellId 를 생성하는 메서드
   getCellId() {
     return makeCellId(this.row, this.column);
   }
 }
 
+/**
+ * Row 와 Column 위치로 Cell 엘리먼트를 가져오는 함수
+ * @param row
+ * @param col
+ * @returns {Element}
+ */
 const getElementFromRowCol = (row, col) => {
   return document.querySelector("#" + makeCellId(row, col));
 };
 
+/**
+ * spreadSheet 의 header 들 active 상태된 것이 있으면 다 지우는 함수
+ */
+const clearHeaderActiveStates = () => {
+  const headers = document.querySelectorAll(".header");
+
+  headers.forEach((header) => header.classList.remove("active"));
+};
+
+/**
+ * 사용자가 셀을 선택했을 때 실행되는 함수
+ * @param cell : 선택한 셀의 Cell 클래스 정보
+ */
 const handleCellClick = (cell) => {
+  // 먼저 이전에 작성된 셀의 active 상태를 다 지운다.
+  clearHeaderActiveStates();
+
+  // 선택한 셀이 위치에 컬럼, 로 헤더의 엘리먼트를 가져와서 active 상태로 만든다.
   const columnHeader = spreadSheet[0][cell.column];
   const rowHeader = spreadSheet[cell.row][0];
 
@@ -54,22 +95,38 @@ const handleCellClick = (cell) => {
   rowHeaderElement.classList.add("active");
 };
 
+
+
+/**
+ * Cell 클래스를 활용해서 Element 생성하는 함수
+ * @param cell
+ * @returns {HTMLInputElement}
+ */
 const createCellElement = (cell) => {
+  // Cell 은 input 엘리먼트로 만든다.
   const cellElement = document.createElement("input");
+
+  // cellElement 속성 부여
   cellElement.className = "cell";
   cellElement.id = cell.getCellId();
   cellElement.value = cell.data;
   cellElement.disabled = cell.disabled;
 
+  // Header 인 Cell 은 구분이 가능하게 class 를 추가로 준다.
   if (cell.isHeader) {
     cellElement.classList.add("header");
   }
 
+  // Cell 엘리먼트의 이벤트 등록
   cellElement.onclick = () => handleCellClick(cell);
 
   return cellElement;
 };
 
+/**
+ * SpreadSheet 를 그린다.
+ * : ROWS, COLUMNS 로 엑셀 레이아웃을 만드는 함수
+ */
 const drawSheet = () => {
   for (let i = 0; i < spreadSheet.length; i++) {
     // Row 를 열개씩 넣기
@@ -78,6 +135,7 @@ const drawSheet = () => {
 
     for (let j = 0; j < spreadSheet[i].length; j++) {
       const cell = spreadSheet[i][j];
+      // 엘리먼트로 Cell 정보가 담긴 input 엘리먼트를 넣는다
       rowContainerElement.append(createCellElement(cell));
     }
 
@@ -85,20 +143,34 @@ const drawSheet = () => {
   }
 };
 
+/**
+ * row 와 column 의 위치에 ㄸ라ㅏ Cell 클래스의 속성 값을 다르게 부여한다.
+ * @param row
+ * @param col
+ * @returns {{rowName: string, columnName: (string|string)}}
+ */
 const getCellPropertyValue = (row, col) => {
   const cellPropertyValue = {
     rowName: row || "",
     columnName: col ? String.fromCharCode(64 + col) : "",
   };
+
+  // row 가 0번째 줄이면? Column Header
   if (row === 0) {
     cellPropertyValue.isHeader = true;
     cellPropertyValue.disabled = true;
     cellPropertyValue.data = cellPropertyValue.columnName;
-  } else if (col === 0) {
+  }
+
+  // column 이 0 번째 줄이면? Row Header
+  else if (col === 0) {
     cellPropertyValue.isHeader = true;
     cellPropertyValue.disabled = true;
     cellPropertyValue.data = cellPropertyValue.rowName;
-  } else {
+  }
+
+  // 그 이외는 모두 사용자가 입력할 수 있는 Cell
+  else {
     cellPropertyValue.isHeader = false;
     cellPropertyValue.disabled = false;
     cellPropertyValue.data = "";
@@ -107,6 +179,9 @@ const getCellPropertyValue = (row, col) => {
   return cellPropertyValue;
 };
 
+/**
+ * Spread Sheet 를 생성하고 DOM 에 그리는 함수
+ */
 const initSpreadSheet = () => {
   for (let row = 0; row < ROWS; row++) {
     const cols = [];
